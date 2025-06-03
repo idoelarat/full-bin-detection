6e33d806c751d52b597120cf86f64f07f4102e5f
Ido <elaratido@gmail.com>
2025-06-03 16:28:46 +0300

    .

--- server/app.js
+++ server/app.js
@@ -3,8 +3,13 @@ import express from 'express'; // Changed from const express = require("express"
 const app = express();
 app.use(express.json());
 
+//connect to db
+
+
 // define a port number and giving us console log messages
 const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+//routes
