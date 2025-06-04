e8f4687a95c202de9b201233227031e09b668034
Ido <elaratido@gmail.com>
2025-06-04 17:41:47 +0300

    .

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import db from './config/db.js';
+//import db from './config/db.js';
 
 const app = express();
 app.use(express.json());
 
 //connect to db
-console.log('App started, DB should be connected.');
+//console.log('App started, DB should be connected.');
 
 // define a port number and giving us console log messages
 const port = 3000;
