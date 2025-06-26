464878670427c82ef450df9f471007ce9bcda2d4
Ido Elarat <elaratido@gmail.com>
2025-06-26 21:15:54 +0300

    changes to the pass

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '12345',
+  password: '1234',
   database: 'full_bin_project'
 });
 
