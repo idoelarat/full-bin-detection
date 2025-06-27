1ad5dc999e46b093daf1e0c00972163734f0f4a3
diana <dianavain01@gmail.com>
2025-06-27 13:19:11 +0300

    Save before checkout

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import db from "./config/db.js";
 import areaRoutes from "./routes/areaRoutes.js";
+import createBinRoutes from './routes/createBinRoutes.js';
+
 
 const app = express();
 app.use(express.json());
@@ -8,7 +10,6 @@ app.use(express.json());
 //connect to db
 console.log("App started, DB should be connected.");
 
-// define a port number and giving us console log messages
 const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
@@ -16,6 +17,12 @@ app.listen(port, () => {
 
 //routes
 app.use("/api/areas", areaRoutes);
+app.use("/api/createBin", createBinRoutes); 
+
+app.use((req, res) => {
+  console.log('404 Not Found:', req.method, req.url);
+  res.status(404).send('Not Found');
+});
 
 // Graceful shutdown of the database connection
 process.on("SIGINT", () => {
