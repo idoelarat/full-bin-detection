f06fc0fda8d36f3615edc27060889e50b8192f79
Liad_V <liad778@logi.com>
2025-08-22 10:58:26 +0300

    first version of list of area components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import AreasSidebar from "./components/areas/AreasSidebar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div></div>
+      <div className="app-container">
+        <div><AreasSidebar buildingNum="1" floor="1" /></div>
+        <div><AreasSidebar buildingNum="1" floor="2" /></div>
+        <div><AreasSidebar buildingNum="4" floor="2" /></div>
+      </div>
       <h1>Full Bin Detection System</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
