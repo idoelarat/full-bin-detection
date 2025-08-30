d669fb5e7ed8917d5ba8160d1b50bd75e5b22223
diana <dianavain01@gmail.com>
2025-08-31 00:18:24 +0300

    work1

--- client/src/components/InfoAreas.jsx
+++ client/src/components/InfoAreas.jsx
@@ -1,4 +1,3 @@
-//InfoAreas.jsx
 import React from "react"
 import InfoBox from "./InfoBox";
 const InfoAreas = ({filteredBins})=>{
@@ -6,27 +5,31 @@ const InfoAreas = ({filteredBins})=>{
     return (
         <div style={{flexDirection:'row',gap:'48px',padding:'4px',display:'flex',justifyContent:'center',alignItems:'center',height:'100%'}}>
              <InfoBox 
-            height="120px" 
-            width="200px" 
-            text={`סה"כ פחים: ${filteredBins.length}`}
-            textColor="#2563eb" 
-            textSize="20px"
+            height="60px" 
+            width="120px" 
+            text={`Total Bins: ${filteredBins.length}`}
+            textColor="#000" 
+            textSize="16px"
             borderSize="1px" 
             borderColor="#2563eb" 
+            backgroundColor="#c4d3f5ff"
+            boxShadow="#217ea554"
+            
           />
 
           <InfoBox 
-            height="120px" 
-            width="200px" 
-            text={`פחים מלאים: ${filteredBins.filter(b => String(b.bin_desc) === "1").length}`}
-            textColor="#dc2626" 
-            textSize="20px"
+            height="60px" 
+            width="120px" 
+            text={`Full Bins: ${filteredBins.filter(b => String(b.bin_desc) === "1").length}`}
+            textColor="#000" 
+            textSize="16px"
             borderSize="1px" 
             borderColor="#dc2626" 
-            isFull={true}
+            backgroundColor="#fec3c3ff"
+            boxShadow="#fd795154"
+
           />
         </div>
     )
 }
 export default InfoAreas;
-
