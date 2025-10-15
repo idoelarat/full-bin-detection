3047e0688a457157a29eac394b451d96f2f6918f
Or Bitton <orbitzx25@gmail.com>
2025-10-15 18:29:20 +0300

    Resolve merge conflict in db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,23 @@
-//config/db.js
+// config/db.js
+
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'full_bin_project'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect(err => {
   if (err) {
-    console.error('❌ MySQL connection failed:', err.stack);
+    console.error('MySQL connection failed:', err.stack);
     return;
   }
-  console.log('✅ Connected to MySQL');
+  console.log('Connected to MySQL');
 });
 
 export default connection;
